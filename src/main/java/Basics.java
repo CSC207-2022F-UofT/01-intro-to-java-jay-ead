d293c283ed7df49c60ffdf596d2726bc4ff41dca
jay-ead <jay.ead@mail.utoronto.ca>
2022-09-22 20:43:37 -0400

    Completed Git Exercise 1

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,7 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,7 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,6 +99,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for(int count = 10; count >= 0; count --){
+            System.out.println("Current count: " + count);
+        }
 
 
     }
@@ -143,6 +146,20 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        char last_char = ' ';
+
+        for(int i = 0; i < to_split.length(); i++){
+
+            char let = to_split.charAt(i);
+
+            if(last_char == ' '){
+                ret.append(let);
+            }
+
+            last_char = let;
+
+        }
+
 
         return ret.toString();
     }
@@ -170,6 +187,14 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+
+        if(arr.length < 2){
+            return 0;
+        }
+
+        for(int i = 1; i < arr.length; i += 2){
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
